00717ae9da3292819945900027aca82871b37bbb
Seon-Il Kim <seon1.kim@samsung.com>
2021-01-26 17:42:19 +0900

    Fix reactionEvent

--- websocket_reactions.go
+++ websocket_reactions.go
@@ -1,7 +1,7 @@
 package slack
 
-// reactionItem is a lighter-weight item than is returned by the reactions list.
-type reactionItem struct {
+// ReactionItem is a lighter-weight item than is returned by the reactions list.
+type ReactionItem struct {
 	Type        string `json:"type"`
 	Channel     string `json:"channel,omitempty"`
 	File        string `json:"file,omitempty"`
@@ -9,17 +9,18 @@ type reactionItem struct {
 	Timestamp   string `json:"ts,omitempty"`
 }
 
-type reactionEvent struct {
+// ReactionEvent is a event that reaction added or removed
+type ReactionEvent struct {
 	Type           string       `json:"type"`
 	User           string       `json:"user"`
 	ItemUser       string       `json:"item_user"`
-	Item           reactionItem `json:"item"`
+	Item           ReactionItem `json:"item"`
 	Reaction       string       `json:"reaction"`
 	EventTimestamp string       `json:"event_ts"`
 }
 
 // ReactionAddedEvent represents the Reaction added event
-type ReactionAddedEvent reactionEvent
+type ReactionAddedEvent ReactionEvent
 
 // ReactionRemovedEvent represents the Reaction removed event
-type ReactionRemovedEvent reactionEvent
+type ReactionRemovedEvent ReactionEvent
